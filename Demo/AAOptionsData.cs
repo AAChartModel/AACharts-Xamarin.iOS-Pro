47b09beea0ca273fa69f18d51d421ff58b86b286
An An <2236368544@qq.com>
2022-04-30 20:10:13 +0800

    Update AAOptionsData.cs

--- Demo/AAOptionsData.cs
+++ Demo/AAOptionsData.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Text;
 using Foundation;
 using Newtonsoft.Json;
@@ -12,93 +13,93 @@ namespace AAChartsDotNet
     public class AAOptionsData
     {
  //注意: 此处的变量全部都会生成默认值(默认全部调用一遍对应的静态方法)
-    public static NSArray variablepieData = getVariablepieData();
-    public static NSArray variwideData = getVariwideData();
-    public static NSArray heatmapData = getHeatmapData();
-    public static NSArray columnpyramidData = getColumnpyramidData();
-    public static NSArray treemapWithColorAxisData = getTreemapWithColorAxisData();
-    public static NSArray drilldownTreemapData = getDrilldownTreemapData();
+    public static Object[] variablepieData = getVariablepieData();
+    public static Object[] variwideData = getVariwideData();
+    public static Object[] heatmapData = getHeatmapData();
+    public static Object[] columnpyramidData = getColumnpyramidData();
+    public static Object[] treemapWithColorAxisData = getTreemapWithColorAxisData();
+    public static Object[] drilldownTreemapData = getDrilldownTreemapData();
 
-    public static NSArray sankeyData = getSankeyData();
-    public static NSArray dependencywheelData = getDependencywheelData();
-    public static NSArray sunburstData = getSunburstData();
-    public static NSArray dumbbellData = getDumbbellData();
-    public static NSArray vennData = getVennData();
-    public static NSArray lollipopData = getLollipopData();
-    public static NSArray tilemapData = getTilemapData();
-    public static NSArray treemapWithLevelsData = getTreemapWithLevelsData();
+    public static Object[] sankeyData = getSankeyData();
+    public static Object[] dependencywheelData = getDependencywheelData();
+    public static Object[] sunburstData = getSunburstData();
+    public static Object[] dumbbellData = getDumbbellData();
+    public static Object[] vennData = getVennData();
+    public static Object[] lollipopData = getLollipopData();
+    public static Object[] tilemapData = getTilemapData();
+    public static Object[] treemapWithLevelsData = getTreemapWithLevelsData();
     public static object[] xrangeData = getXrangeData();
-    public static NSArray vectorData = getVectorData();
-    public static NSArray bellcurveData = getBellcurveData();
-    public static NSArray timelineData = getTimelineData();
-    public static NSArray itemData = getItemData();
-    public static NSArray windbarbData = getWindbarbData();
-    public static NSArray networkgraphData = getNetworkgraphData();
-    public static NSArray wordcloudData = getWordcloudData();
-    public static NSArray eulerData = getEulerData();
+    public static Object[] vectorData = getVectorData();
+    public static Object[] bellcurveData = getBellcurveData();
+    public static Object[] timelineData = getTimelineData();
+    public static Object[] itemData = getItemData();
+    public static Object[] windbarbData = getWindbarbData();
+    public static Object[] networkgraphData = getNetworkgraphData();
+    public static Object[] wordcloudData = getWordcloudData();
+    public static Object[] eulerData = getEulerData();
 
-    public static NSArray organizationData = OrganizationData();
-    public static NSArray organizationNodesData = OrganizationNodesData();
+    public static Object[] organizationData = OrganizationData();
+    public static Object[] organizationNodesData = OrganizationNodesData();
 
-    public static NSArray arcdiagram1Data = Arcdiagram1Data();
-    public static NSArray arcdiagram2Data = Arcdiagram2Data();
-    public static NSArray arcdiagram3Data = Arcdiagram3Data();
-    public static NSArray flameData = FlameData();
-    public static NSArray sunburst2Data = Sunburst2Data();
+    public static Object[] arcdiagram1Data = Arcdiagram1Data();
+    public static Object[] arcdiagram2Data = Arcdiagram2Data();
+    public static Object[] arcdiagram3Data = Arcdiagram3Data();
+    public static Object[] flameData = FlameData();
+    public static Object[] sunburst2Data = Sunburst2Data();
 
-    private static NSArray getVariablepieData() {
+    private static Object[] getVariablepieData() {
         return getJsonDataWithJsonFileName("variablepieData");
     }
 
-    private static NSArray getVariwideData() {
+    private static Object[] getVariwideData() {
         return getJsonDataWithJsonFileName("variwideData");
     }
 
-    private static NSArray getHeatmapData() {
+    private static Object[] getHeatmapData() {
         return getJsonDataWithJsonFileName("heatmapData");
     }
 
-    private static NSArray getColumnpyramidData() {
+    private static Object[] getColumnpyramidData() {
         return getJsonDataWithJsonFileName("columnpyramidData");
     }
 
-    private static NSArray getTreemapWithColorAxisData() {
+    private static Object[] getTreemapWithColorAxisData() {
         return getJsonDataWithJsonFileName("treemapWithColorAxisData");
     }
 
-    private static NSArray getDrilldownTreemapData() {
+    private static Object[] getDrilldownTreemapData() {
         return getJsonDataWithJsonFileName("drilldownTreemapData");
     }
 
-    private static NSArray getSankeyData() {
+    private static Object[] getSankeyData() {
         return getJsonDataWithJsonFileName("sankeyData");
     }
 
-    private static NSArray getDependencywheelData() {
+    private static Object[] getDependencywheelData() {
         return getJsonDataWithJsonFileName("dependencywheelData");
     }
 
-    private static NSArray getSunburstData() {
+    private static Object[] getSunburstData() {
         return getJsonDataWithJsonFileName("sunburstData");
     }
 
-    private static NSArray getDumbbellData() {
+    private static Object[] getDumbbellData() {
         return getJsonDataWithJsonFileName("dumbbellData");
     }
 
-    private static NSArray getVennData() {
+    private static Object[] getVennData() {
         return getJsonDataWithJsonFileName("vennData");
     }
 
-    private static NSArray getLollipopData() {
+    private static Object[] getLollipopData() {
         return getJsonDataWithJsonFileName("lollipopData");
     }
 
-    private static NSArray getTilemapData() {
+    private static Object[] getTilemapData() {
         return getJsonDataWithJsonFileName("tilemapData");
     }
 
-    private static NSArray getTreemapWithLevelsData() {
+    private static Object[] getTreemapWithLevelsData() {
         return getJsonDataWithJsonFileName("treemapWithLevelsData");
     }
 
@@ -113,84 +114,89 @@ namespace AAChartsDotNet
         return dataArr.ToArray();
     }
 
-    private static NSArray getVectorData() {
+    private static Object[] getVectorData() {
         return getJsonDataWithJsonFileName("vectorData");
     }
 
-    private static NSArray getBellcurveData() {
+    private static Object[] getBellcurveData() {
         return getJsonDataWithJsonFileName("bellcurveData");
     }
 
-    private static NSArray getTimelineData() {
+    private static Object[] getTimelineData() {
         return getJsonDataWithJsonFileName("timelineData");
     }
 
-    private static NSArray getItemData() {
+    private static Object[] getItemData() {
         return getJsonDataWithJsonFileName("itemData");
     }
 
-    private static NSArray getWindbarbData() {
+    private static Object[] getWindbarbData() {
         return getJsonDataWithJsonFileName("windbarbData");
     }
 
-    private static NSArray getNetworkgraphData() {
+    private static Object[] getNetworkgraphData() {
         return getJsonDataWithJsonFileName("networkgraphData");
     }
 
-    private static NSArray getWordcloudData() {
+    private static Object[] getWordcloudData() {
         return getJsonDataWithJsonFileName("wordcloudData");
     }
 
-    private static NSArray getEulerData() {
+    private static Object[] getEulerData() {
         return getJsonDataWithJsonFileName("eulerData");
     }
 
-    public static NSArray OrganizationData() {
+    public static Object[] OrganizationData() {
         return getJsonDataWithJsonFileName("organizationData");
     }
 
-    public static NSArray OrganizationNodesData() {
+    public static Object[] OrganizationNodesData() {
         return getJsonDataWithJsonFileName("organizationNodesData");
     }
 
 
-    public static NSArray Arcdiagram1Data() {
+    public static Object[] Arcdiagram1Data() {
         return getJsonDataWithJsonFileName("arcdiagram1Data");
     }
 
-    public static NSArray Arcdiagram2Data() {
+    public static Object[] Arcdiagram2Data() {
         return getJsonDataWithJsonFileName("arcdiagram2Data");
     }
 
-    public static NSArray Arcdiagram3Data() {
+    public static Object[] Arcdiagram3Data() {
         return getJsonDataWithJsonFileName("arcdiagram3Data");
     }
 
-    public static NSArray FlameData() {
+    public static Object[] FlameData() {
         return getJsonDataWithJsonFileName("flameData");
     }
 
-    public static NSArray Sunburst2Data() {
+    public static Object[] Sunburst2Data() {
         return getJsonDataWithJsonFileName("sunburst2Data");
     }
 
-    private static NSArray getJsonDataWithJsonFileName(String jsonFileName) {
+    private static Object[] getJsonDataWithJsonFileName(String jsonFileName) {
         var fileName = @$"Demo/DataJson/{jsonFileName}.json";
         var localJsonPath = Path.Combine(NSBundle.MainBundle.BundlePath, fileName);
         var jsonData = NSData.FromFile(localJsonPath);
         NSError error;
         var jsonObj = NSJsonSerialization.Deserialize(jsonData, NSJsonReadingOptions.MutableContainers, out error);
-        if (jsonData == null || error != null) 
-        {
-           Console.WriteLine(@$"失败❌❌❌ 📃JSON文件{jsonFileName}解码失败");
-           return null;
-        }
-        else
-        {
+        // if (jsonData == null || error != null) 
+        // {
+        //    Console.WriteLine(@$"失败❌❌❌ 📃JSON文件{jsonFileName}解码失败");
+        //    return null;
+        // }
+        // else
+        // {
             Console.WriteLine(@$"成功🔥🔥🔥 📃JSON文件{jsonFileName}解码成功");
-            NSArray jsonArr = NSArray.FromNSObjects(jsonObj);
-            return jsonArr;
-        }
+            var jsonArr = NSArray.FromNSObjects(jsonObj);
+            //https://stackoverflow.com/questions/31679955/how-do-i-cast-my-nsarray-to-liststring-in-xamarin-ios
+            var finalJsonArr = Enumerable.Range(0, (int)((NSArray)jsonArr).Count)
+                .Select(i => ((NSArray)jsonArr)
+                    .GetItem<NSObject>(nuint.Parse(i.ToString())).ToString())
+                .ToArray();
+            return finalJsonArr;
+        // }
     }
     
 //     + (id)getJsonDataWithJsonFileName:(NSString *)jsonFileName
